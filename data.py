11784ebb964ce7fff3e3607a295bd0c9e418736a
Jithu L Devaraj <jithuldevaraj01@gmail.com>
2024-09-18 00:48:47 +0530

    Final (1st)

--- data.py
+++ data.py
@@ -14,3 +14,38 @@ question_data = [
     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
 ]
+
+# question_data = [{"type": "boolean", "difficulty": "easy", "category": "History",
+#                   "question": "Adolf Hitler was a german soldier in World War I.",
+#                   "correct_answer": "True", "incorrect_answers": ["False"]},
+#                  {"type": "boolean", "difficulty": "easy", "category": "History",
+#                   "question": "In World War ll, Great Britian used inflatable tanks on the "
+#                                "ports of Great Britain to divert Hitler away from Normandy\\/D-day landing.",
+#                    "correct_answer": "True", "incorrect_answers": ["False"]},
+#                  {"type": "boolean", "difficulty": "easy", "category": "History",
+#                    "question": "Adolf Hitler was tried at the Nuremberg trials.",
+#                    "correct_answer": "False", "incorrect_answers": ["True"]},
+#                  {"type": "boolean", "difficulty": "easy", "category": "History",
+#                    "question": "The United States of America was the first country to launch a man into space.",
+#                    "correct_answer": "False", "incorrect_answers": ["True"]},
+#                  {"type": "boolean", "difficulty": "easy", "category": "History",
+#                    "question": "The United States was a member of the League of Nations.",
+#                    "correct_answer": "False", "incorrect_answers": ["True"]},
+#                  {"type": "boolean", "difficulty": "easy", "category": "History",
+#                    "question": "The United States Department of Homeland Security was formed in response to the "
+#                                "September 11th attacks.",
+#                    "correct_answer": "True", "incorrect_answers": ["False"]},
+#                   {"type": "boolean", "difficulty": "easy", "category": "History",
+#                    "question": "The French Kingdom helped the United States gain their independence over Great Britain "
+#                                "during the Revolutionary War.",
+#                    "correct_answer": "True", "incorrect_answers": ["False"]},
+#                   {"type": "boolean", "difficulty": "easy", "category": "History",
+#                    "question": "Thomas Crapper was a plumber who invented the flushing toilet.",
+#                    "correct_answer": "False", "incorrect_answers": ["True"]},
+#                   {"type": "boolean", "difficulty": "easy", "category": "History",
+#                    "question": "Kublai Khan is the grandchild of Genghis Khan?",
+#                    "correct_answer": "True", "incorrect_answers": ["False"]},
+#                   {"type": "boolean", "difficulty": "easy", "category": "History",
+#                    "question": "Former United States Presidents John Adams and Thomas Jefferson died within hours of "
+#                                "each other.",
+#                    "correct_answer": "True", "incorrect_answers": ["False"]}]
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
-# Create a question object
+# Creating a question object List.
 question_bank = []
 for question in question_data:
     temp_question_obj = Question(question["text"], question["answer"])  # Creating a Question Object.
     question_bank.append(temp_question_obj)  # Append each question object to question_bank list.
 
-print(question_bank[0].text) 
-print(question_bank[0].answer)
+obj_quiz_brain = QuizBrain(question_bank)
+while obj_quiz_brain.still_has_questions():
+    obj_quiz_brain.next_question()
 
-
-
-
+print("You've completed the quiz")
+print(f"Your final score was: {obj_quiz_brain.score}/{obj_quiz_brain.question_number}")
